5a3abe4b3f01164547d5ab460a18266e188f9305
Pooja Vishwakarma <123650050+PoojaVishwakarma0402@users.noreply.github.com>
2024-01-06 07:28:31 +0000

    Brief description of changes

--- Datatypes.js
+++ Datatypes.js
@@ -1,4 +1,16 @@
 "use strict"  // teat all JS code as newer version
 
 
-alert(3+3)  // we are using nodejs not browser hence it shows error
+// alert(3+3)  // we are using nodejs not browser hence it shows error
+
+// primitive datatypes
+// 1.number=> 2^53(range)
+// bigint for big datatypes(not used mostly)
+// boolean
+// null=? standalone value (it is an object)
+// undefined=> for values that are not defined (type is undefined)
+// symbol=>to find uniqueness
+// string
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);  //object
